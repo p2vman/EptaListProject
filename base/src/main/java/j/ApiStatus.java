1cdd60934d133082e9ca73d1efa9767fb200f2fb
p2vman <andreywontomsky@gmail.com>
2025-03-28 20:30:46 +0200

    and minor changes

--- base/src/main/java/j/ApiStatus.java
+++ base/src/main/java/j/ApiStatus.java
@@ -1,6 +1,13 @@
 package j;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 public class ApiStatus {
+    @Retention(RetentionPolicy.SOURCE)
+    @Target({ElementType.CONSTRUCTOR, ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PACKAGE})
     public @interface In_Development {
 
     }
